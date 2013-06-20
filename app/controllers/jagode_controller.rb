67a57bfa2be415858b0f42c78cec479e09174c7e
goran <goran.pizent@gmail.com>
2013-06-20 13:26:23 +0200

    import from file added

--- app/controllers/jagode_controller.rb
+++ app/controllers/jagode_controller.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 class JagodeController < ApplicationController
 
 	def index
@@ -8,6 +10,35 @@ class JagodeController < ApplicationController
 		@tac = 273.63 - 55.23*(params[:sorta].to_i) + 78.22*(params[:tip].to_i) - 14.96*params[:param_a].to_f + 12.32*params[:param_b].to_f
 		@rezultat = @tac.round(2)
 		end
+
+	end
+
+	def calculate(sorta, tip, param_a, param_b)
+		(273.63 - 55.23*(sorta.to_i) + 78.22*(tip.to_i) - 14.96*(param_a.to_f) + 12.32*(param_b.to_f)).round(2)
+	end
+
+	def import
+		
+		@result = file_upload() if request.post?
+
+	end
+
+	def file_upload
+	  
+	  tmp = params[:file_upload][:my_file].tempfile
+
+		require "csv"
+		parsed_file = CSV.read(tmp, { :col_sep => "\t" })
+		
+		@result =[]
+
+		parsed_file.each{|row|
+			next if row.nil? || row[0].nil? || [0][0].nil? || row[0][0]=='#'
+			result = calculate(row[0],row[1],row[2],row[3])
+			@result << [result,row[0],row[1],row[2],row[3]]
+			#logger.debug("TAC = #{result} -> Cultivars = #{row[0]} | Form = #{row[2]} | A = #{row[2]} | B = #{row[3]}")
+		}
+		@result
 
 	end
 
