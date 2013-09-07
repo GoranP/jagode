fb45ede1f4f4b57c41ba89c60bb9411d255d7686
goran <goran.pizent@gmail.com>
2013-09-07 16:51:29 +0200

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,25 @@
-set :application, "jagode"
-set :repository,  "https://github.com/GoranP/jagode"
+server "strawberry.pbf.hr", :app, :web, :db, :primary => true
 
-role :web, "nginx"                          # Your HTTP server, Apache/etc
-role :app, "passenger"                          # This may be the same as your `Web` server
+set :application, "jagode"
+set :repository,  "https://github.com/GoranP/jagode.git"
+set :scm, "git"
+set :user, "goran"  # The server's user for deploys
+set :rails_env, "production"
+
+set :deploy_to, "/var/www/strawberry"
+
+set :branch, "master"
+set :deploy_via, :remote_cache
+
+set :keep_releases, 5
+
+#role :web, "nginx"                          # Your HTTP server, Apache/etc
+#role :app, "passenger"                          # This may be the same as your `Web` server
 #role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
+
+default_run_options[:pty] = true 
+set :ssh_options, { :forward_agent => true }
 
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
