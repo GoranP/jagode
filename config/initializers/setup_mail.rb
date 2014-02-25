9148874206fb2bc36fe88735a7031e110e2f4118
goran <goran.pizent@gmail.com>
2014-02-25 23:56:28 +0100

    email settings

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,5 +1,4 @@
-
 email_settings = YAML::load(File.open("#{Rails.root.to_s}/config/mail.yml"))
-ActionMailer::Base.smtp_settings = email_settings[Rails.env]["smtp_setttings"] unless email_settings[Rails.env].nil?
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.default_options = email_settings[Rails.env]["default_options"].to_hash  
+ActionMailer::Base.smtp_settings = email_settings[Rails.env]["smtp_settings"] unless email_settings[Rails.env].nil?
+ActionMailer::Base.default_options = email_settings[Rails.env]["default_options"].to_hash
+
